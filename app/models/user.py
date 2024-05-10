66f8f4ea5cb8f5cd3f3a1a354cd4a2dd641aff6d
kevin <kmtsou@sbcglobal.net>
2024-05-09 21:01:11 -0700

    removing tkinter import

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,4 @@
-from tkinter import CASCADE
+# from tkinter import CASCADE
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
